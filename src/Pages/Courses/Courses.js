0941442de0d446ca11bc92c398073a327e6f9e4f
unknown <you@example.com>
2022-10-25 15:58:39 +0600

    Course Detail Page Content Added

--- src/Pages/Courses/Courses.js
+++ src/Pages/Courses/Courses.js
@@ -1,41 +1,49 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import { FaStar, FaVideo, FaBook, FaCertificate } from 'react-icons/fa';
+import { FaStar, FaVideo, FaBook, FaCertificate, FaCalendarTimes, FaLanguage, FaCheck, FaCheckSquare } from 'react-icons/fa';
 
 const Courses = () => {
     const courseDetail = useLoaderData();
-    const { title, detail, overview, rating, enrolled, author, sections, published_date, lectures,  total_length, video_length, article_number, certificate, content } = courseDetail;
+    const { title, detail, overview, language, rating, enrolled, author, sections, published_date, lectures, total_length, video_length, article_number, certificate, content } = courseDetail;
     return (
         <div>
-            <div className='bg-gray-800 text-white'>
-                <h1>{title}</h1>
-                <p>{detail}</p>
-                <div className='flex gap-5 items-center'>
-                    <p>Bestseller</p>
-                    <div className='flex items-center gap-2'>
-                        <FaStar></FaStar>
-                        <p>{rating}</p>
+            <div className='bg-gray-800 text-white p-5'>
+                <div className='container mx-auto'>
+                    <h1 className='text-3xl font-bold mb-5'>{title}</h1>
+                    <p className='text-xl font-normal mb-2'>{detail}</p>
+                    <div className='flex gap-5 items-center mb-2'>
+                        <p className='bg-amber-400 p-1'>Bestseller</p>
+                        <div className='flex items-center gap-2'>
+                            <FaStar></FaStar>
+                            <p>{rating}</p>
+                        </div>
+                        <p>{enrolled} Students</p>
                     </div>
-                    <p>{enrolled} Students</p>
-                </div>
-                <p>Created By {author}</p>
-                <div>
-
-                    <p>Publish Date {published_date}</p>
+                    <p className='mb-2'>Created By <span className='text-amber-500'>{author}</span></p>
+                    <div className='flex gap-5'>
+                        <div className='flex items-center gap-2'>
+                            <FaCalendarTimes></FaCalendarTimes>
+                            <p>Publish Date {published_date}</p>
+                        </div>
+                        <div className='flex items-center gap-2'>
+                            <FaLanguage></FaLanguage>
+                            <p>Language {language}</p>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div>
-                <h2>Course Overview</h2>
-                <div>
+            <div className='container mx-auto mt-10'>
+                <h2 className='text-2xl font-bold mb-5'>Course Overview</h2>
+                <div className='mb-10'>
                     {
                         overview.map(item =>
-                            <ul>
-                                <li>{item}</li>
+                            <ul className='flex gap-5 mb-2 items-center'>
+                                <FaCheck></FaCheck><li>{item}</li>
                             </ul>
                         )
                     }
                 </div>
-                <div className='flex gap-3'>
+                <div className='flex gap-3 mb-10'>
                     <div className='border p-5 flex flex-col gap-5 items-center'>
                         <FaVideo></FaVideo>
                         <p>{video_length} hours of video</p>
@@ -50,21 +58,24 @@ const Courses = () => {
                     </div>
                 </div>
                 <div>
-                    <h2>Course Content</h2>
+                    <h2 className='text-2xl font-bold mb-5'>Course Content</h2>
                     <div className='flex'>
                         <p>{sections} sections</p>
                         <p>{lectures} lectures</p>
                         <p>{total_length} total length</p>
                     </div>
+                    <div className='mt-5'>
                         {
-                            content.map(contentList => 
-                              <ul>
-                                <li>
-                                    {contentList}
-                                </li>
-                              </ul>  
-                                )
+                            content.map(contentList =>
+                                <ul className='flex items-center gap-10 border p-3 mb-2 bg-slate-200 hover:bg-slate-400 hover:text-white'>
+                                    <FaCheckSquare></FaCheckSquare>
+                                    <li className='font-medium text-lg'>
+                                        {contentList}
+                                    </li>
+                                </ul>
+                            )
                         }
+                    </div>
                 </div>
             </div>
         </div>
