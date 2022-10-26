5b3c32eb8e16b1772818c682cc02d858c62eb752
unknown <you@example.com>
2022-10-26 15:57:50 +0600

    Checkout Page Complete

--- src/Pages/PremiumAccessForm/PremiumAccessForm.js
+++ src/Pages/PremiumAccessForm/PremiumAccessForm.js
@@ -1,81 +1,126 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useLoaderData } from 'react-router-dom';
+import { FaStar, FaGoogle } from 'react-icons/fa';
 
 const PremiumAccessForm = () => {
     const courseData = useLoaderData();
-    const { title, detail } = courseData;
+    const { _id, title, detail, price, image, rating } = courseData;
+    const countries = ["China", "Russia", "UK"];
+    const [menu, setMenu] = useState(false);
+    const [country, setCountry] = useState("United States");
+
+    const changeText = (e) => {
+        setMenu(false);
+        setCountry(e.target.textContent);
+    };
+
     return (
-        <div className=' bg-white container mx-auto'>
-            <h1 className='text-3xl font-bold text-white text-center mb-3 pt-10'>Purchase Course: <span className='text-amber-500 text-4xl'>{title}</span></h1>
-            <p className='font-medium text-center text-black'>{detail}</p>
-            <div className='mb-20'>
-                <section className="p-6 bg-white dark:text-gray-50">
-                    <form novalidate="" action="" className="container flex flex-col mx-auto space-y-12 ng-untouched ng-pristine ng-valid">
-                        <fieldset className="grid grid-cols-4 gap-6 p-6 rounded-md shadow-sm bg-amber-800">
-                            <div className="space-y-2 col-span-full lg:col-span-1">
-                                <p className="font-medium">Personal Inormation</p>
-                            </div>
-                            <div className="grid grid-cols-6 gap-4 col-span-full lg:col-span-3">
-                                <div className="col-span-full sm:col-span-3">
-                                    <label for="firstname" className="text-sm">First name</label>
-                                    <input id="firstname" type="text" placeholder="First name" className="w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-violet-400 dark:border-gray-700 dark:text-gray-900" />
+        <div className="flex justify-center items-center">
+            <div className="py-16 px-4 md:px-6 2xl:px-0 flex justify-center items-center 2xl:mx-auto 2xl:container">
+                <div className="flex flex-col justify-start items-start w-full space-y-9">
+                    <div className="flex justify-start flex-col items-start space-y-2">
+                        <button className="flex flex-row items-center text-gray-600 hover:text-gray-500 space-x-1">
+                            <svg className="fill-stroke" width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M2.91681 7H11.0835" stroke="currentColor" strokeWidth="0.666667" strokeLinecap="round" strokeLinejoin="round" />
+                                <path d="M2.91681 7L5.25014 9.33333" stroke="currentColor" strokeWidth="0.666667" strokeLinecap="round" strokeLinejoin="round" />
+                                <path d="M2.91681 7.00002L5.25014 4.66669" stroke="currentColor" strokeWidth="0.666667" strokeLinecap="round" strokeLinejoin="round" />
+                            </svg>
+                            <Link to='/'><p className="text-sm leading-none">Home</p></Link>
+                        </button>
+                        <p className="text-3xl lg:text-4xl font-semibold leading-7 lg:leading-9 text-gray-800">Checkout</p>
+                    </div>
+
+                    <div className="flex flex-col xl:flex-row justify-center xl:justify-between space-y-6 xl:space-y-0 xl:space-x-6 w-full">
+                        <div className="max-w-lg p-8 h-3/6 shadow-md bg-stone-300 dark:text-gray-100">
+                            <div className="space-y-4">
+                                <div className="space-y-2">
+                                    <Link to={`/premium-access/${_id}`}>
+                                        <img src={image} alt="" className="block object-cover object-center w-full mb-10 rounded-md dark:bg-gray-500" />
+                                    </Link>
                                 </div>
-                                <div className="col-span-full sm:col-span-3">
-                                    <label for="lastname" className="text-sm">Last name</label>
-                                    <input id="lastname" type="text" placeholder="Last name" className="w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-violet-400 dark:border-gray-700 dark:text-gray-900" />
+                                <div className="space-y-2">
+                                    <Link to={`/premium-access/${_id}`} rel="noopener noreferrer" href="#" className="block">
+                                        <h3 className="text-2xl font-bold text-amber-500">{title}</h3>
+                                    </Link>
+                                    <p className="leading-snug text-black">{detail}</p>
                                 </div>
-                                <div className="col-span-full sm:col-span-3">
-                                    <label for="email" className="text-sm">Email</label>
-                                    <input id="email" type="email" placeholder="Email" className="w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-violet-400 dark:border-gray-700 dark:text-gray-900" />
-                                </div>
-                                <div className="col-span-full">
-                                    <label for="address" className="text-sm">Address</label>
-                                    <input id="address" type="text" placeholder="" className="w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-violet-400 dark:border-gray-700 dark:text-gray-900" />
-                                </div>
-                                <div className="col-span-full sm:col-span-2">
-                                    <label for="city" className="text-sm">City</label>
-                                    <input id="city" type="text" placeholder="" className="w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-violet-400 dark:border-gray-700 dark:text-gray-900" />
-                                </div>
-                                <div className="col-span-full sm:col-span-2">
-                                    <label for="state" className="text-sm">State / Province</label>
-                                    <input id="state" type="text" placeholder="" className="w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-violet-400 dark:border-gray-700 dark:text-gray-900" />
-                                </div>
-                                <div className="col-span-full sm:col-span-2">
-                                    <label for="zip" className="text-sm">ZIP / Postal</label>
-                                    <input id="zip" type="text" placeholder="" className="w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-violet-400 dark:border-gray-700 dark:text-gray-900" />
-                                </div>
-                            </div>
-                        </fieldset>
-                        <fieldset className="grid grid-cols-4 gap-6 p-6 rounded-md shadow-sm bg-amber-800">
-                            <div className="space-y-2 col-span-full lg:col-span-1">
-                                <p className="font-medium">Profile</p>
-                            </div>
-                            <div className="grid grid-cols-6 gap-4 col-span-full lg:col-span-3">
-                                <div className="col-span-full sm:col-span-3">
-                                    <label for="username" className="text-sm">Username</label>
-                                    <input id="username" type="text" placeholder="Username" className="w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-violet-400 dark:border-gray-700 dark:text-gray-900" />
-                                </div>
-                                <div className="col-span-full sm:col-span-3">
-                                    <label for="website" className="text-sm">Website</label>
-                                    <input id="website" type="text" placeholder="https://" className="w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-violet-400 dark:border-gray-700 dark:text-gray-900" />
-                                </div>
-                                <div className="col-span-full">
-                                    <label for="bio" className="text-sm">Bio</label>
-                                    <textarea id="bio" placeholder="" className="w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-violet-400 dark:border-gray-700 dark:text-gray-900"></textarea>
-                                </div>
-                                <div className="col-span-full">
-                                    <label for="bio" className="text-sm">Photo</label>
-                                    <div className="flex items-center space-x-2">
-                                        <img src="https://source.unsplash.com/30x30/?random" alt="" className="w-10 h-10 rounded-full dark:bg-gray-500 dark:bg-gray-700" />
-                                        <button type="button" className="px-4 py-2 border rounded-md dark:border-gray-100">Change</button>
+                                <div className='flex justify-between'>
+                                    <p className='text-black'>Price: $ <span className='text-2xl font-bold text-amber-500'>{price}</span></p>
+                                    <div className='flex items-center gap-2'>
+                                        <p className='text-black'>{rating}</p>
+                                        <div>
+                                            <FaStar className='text-amber-500'></FaStar>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </fieldset>
-                    </form>
-                </section>
-                <div className='flex justify-center mt-10'>
-                    <button className="btn btn-wide bg-amber-700 text-white hover:bg-amber-900">Complete Order</button>
+                        </div>
+
+                        <div className="p-8 bg-gray-100 flex flex-col lg:w-full xl:w-3/5">
+                            <button className="border border-transparent hover:border-gray-300 bg-amber-700 hover:bg-white text-white hover:text-gray-900 flex flex-row justify-center items-center space-x-2 py-4 rounded w-full">
+                                <div>
+                                    <FaGoogle></FaGoogle>
+                                </div>
+                                <div>
+                                    <p className="text-base leading-4">Pay</p>
+                                </div>
+                            </button>
+
+                            <div className="flex flex-row justify-center items-center mt-6">
+                                <hr className="border w-full" />
+                                <p className="flex flex-shrink-0 px-4 text-base leading-4 text-gray-600">or pay with card</p>
+                                <hr className="border w-full" />
+                            </div>
+
+                            <div className="mt-8">
+                                <input className="border border-gray-300 p-4 rounded w-full text-base leading-4 placeholder-gray-600 text-gray-600" type="email" placeholder="Email" />
+                            </div>
+
+                            <label className="mt-8 text-base leading-4 text-gray-800">Card details</label>
+                            <div className="mt-2 flex-col">
+                                <div>
+                                    <input className="border rounded-tl rounded-tr border-gray-300 p-4 w-full text-base leading-4 placeholder-gray-600 text-gray-600" type="email" placeholder="0000 1234 6549 15151" />
+                                </div>
+                                <div className="flex-row flex">
+                                    <input className="border rounded-bl border-gray-300 p-4 w-full text-base leading-4 placeholder-gray-600 text-gray-600" type="email" placeholder="MM/YY" />
+                                    <input className="border rounded-br border-gray-300 p-4 w-full text-base leading-4 placeholder-gray-600 text-gray-600" type="email" placeholder="CVC" />
+                                </div>
+                            </div>
+
+                            <label className="mt-8 text-base leading-4 text-gray-800">Name on card</label>
+                            <div className="mt-2 flex-col">
+                                <div>
+                                    <input className="border rounded border-gray-300 p-4 w-full text-base leading-4 placeholder-gray-600 text-gray-600" type="email" placeholder="Name on card" />
+                                </div>
+                            </div>
+
+                            <label className="mt-8 text-base leading-4 text-gray-800">Country or region</label>
+                            <div className="mt-2 flex-col">
+                                <div className="relative">
+                                    <button className="text-left border rounded-tr rounded-tl border-gray-300 p-4 w-full text-base leading-4 placeholder-gray-600 text-gray-600 bg-white" type="email">
+                                        {country}
+                                    </button>
+                                    <svg onClick={() => setMenu(!menu)} className={"transform  cursor-pointer absolute top-4 right-4 " + (menu ? "rotate-180" : "")} width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M3.5 5.75L8 10.25L12.5 5.75" stroke="#27272A" strokeLinecap="round" strokeLinejoin="round" />
+                                    </svg>
+                                    <div className={"mt-1 absolute z-10 w-full flex bg-gray-50 justify-start flex-col text-gray-600 " + (menu ? "block" : "hidden")}>
+                                        {countries.map((country) => (
+                                            <div key={country} className="cursor-pointer hover:bg-gray-800 hover:text-white px-4 py-2" onClick={changeText}>
+                                                {country}
+                                            </div>
+                                        ))}
+                                    </div>
+                                </div>
+                                <input className="border rounded-bl rounded-br border-gray-300 p-4 w-full text-base leading-4 placeholder-gray-600 text-gray-600" type="text" placeholder="ZIP" />
+                            </div>
+
+                            <button className="mt-8 border border-transparent hover:border-gray-300 bg-amber-700 hover:bg-white text-white hover:text-gray-900 flex justify-center items-center py-4 rounded w-full">
+                                <div>
+                                    <p className="text-base leading-4">Pay $ {price}</p>
+                                </div>
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
